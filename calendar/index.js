f6a0235008cd8c617411206df8327593d6bd805b
DidarHalla <didarhallayev255@gmail.com>
2023-08-17 12:22:56 +0300

    min fix

--- calendar/index.js
+++ calendar/index.js
@@ -5,20 +5,15 @@ function generateCalendar() {
     const yearInput = document.getElementById('year');
     const calendarBody = document.getElementById('calendar-body');
   
-    // Очистка календаря
     calendarBody.innerHTML = '';
   
-    // Получение выбранного месяца и года
     const month = parseInt(monthSelect.value);
     const year = parseInt(yearInput.value);
   
-    // Получение первого дня месяца
     const firstDay = new Date(year, month, 1).getDay();
   
-    // Получение количества дней в месяце
     const daysInMonth = new Date(year, month + 1, 0).getDate();
   
-    // Генерация календаря
     let date = 1;
   
     for (let i = 0; i < 6; i++) {
@@ -26,14 +21,11 @@ function generateCalendar() {
   
       for (let j = 0; j < 7; j++) {
         if (i === 0 && j < firstDay-1) {
-          // Пустые ячейки до первого дня месяца
           const cell = document.createElement('td');
           row.appendChild(cell);
         } else if (date > daysInMonth) {
-          // Пустые ячейки после последнего дня месяца
           break;
         } else {
-          // Ячейки с датами
           const cell = document.createElement('td');
           cell.textContent = date;
           row.appendChild(cell);
