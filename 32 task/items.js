ec3cbf86a03311f4084530f9bc4750b64d5a8d92
DidarHalla <didarhallayev255@gmail.com>
2023-08-28 22:22:31 +0300

    task 32

---
+++ 32 task/items.js
@@ -0,0 +1,20 @@
+export let cards = [
+    {
+        name: 'kap',
+        image: 'https://italy-caps.by/sites/default/files/styles/default/public/new-york.jpg?itok=d35rE1xF',
+        price: '10$',
+        add: 'Добавить в корзину',
+    },
+    {
+        name: 'kap',
+        image: 'https://podsekai.by/image/cache/data/ODEZDA/Kepki,%20shliapi/rapala-camo-led-cap3-1500x1500.jpg',
+        price: '13$',
+        add: 'Добавить в корзину',
+    },
+    {
+        name: 'kap',
+        image: 'https://textile.by/image/cache/catalog/bejsbolki/2026.29MAIN-500x500.jpg',
+        price: '11$',
+        add: 'Добавить в корзину',
+    },
+]
